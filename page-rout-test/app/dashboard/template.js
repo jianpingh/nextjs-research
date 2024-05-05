8b8583a6e4e9ccc3156b979d570791e938f7e981
huang jianping <josam.huang@hotmail.com>
2024-05-05 22:45:14 +0800

    layout and template

---
+++ page-rout-test/app/dashboard/template.js
@@ -0,0 +1,9 @@
+'use client'
+import { useState } from 'react'
+export default function Template({ children }) {
+    const [count, setCount] = useState(0)
+    return ( <>
+               <h1>Template {count}</h1>
+               <button onClick={() => setCount(count + 1)}> Increment2 </button> {children} </>
+    )
+}
