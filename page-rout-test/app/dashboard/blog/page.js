f252dc8ec96c6f9dd806567c4d277bca8931b24a
huang jianping <josam.huang@hotmail.com>
2024-05-06 19:05:22 +0800

    font and css test

---
+++ page-rout-test/app/dashboard/blog/page.js
@@ -0,0 +1,4 @@
+import { notFound } from 'next/navigation'
+export default function Page() {
+    return <h1>This is Blog</h1>
+}
---
+++ page-rout-test/app/dashboard/loading.js
@@ -0,0 +1,1 @@
+export default function DashboardLoading() { return <>Loading dashboard...</> }
